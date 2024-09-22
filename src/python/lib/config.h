252dffe35260dca5b963ada1b83d6a0be37f94c1
a-ma72 <andi.martin@freenet.de>
2024-09-22 23:00:48 +0200

    Version number

--- src/python/lib/config.h
+++ src/python/lib/config.h
@@ -2,8 +2,9 @@
 #define OFF 0
 
 #ifndef RFC_VERSION_MAJOR
-  #define RFC_VERSION_MAJOR         "4"
-  #define RFC_VERSION_MINOR         "7"
+  #define RFC_VERSION_MAJOR         "0"
+  #define RFC_VERSION_MINOR         "5"
+  #define RFC_VERSION_PATCH         "0"
   #define RFC_USE_INTEGRAL_COUNTS    OFF
   #define RFC_USE_HYSTERESIS_FILTER  ON
   #define RFC_MINIMAL                OFF
